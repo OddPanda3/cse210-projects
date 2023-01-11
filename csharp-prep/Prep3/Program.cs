54c7f017cdaa0ccb2eb8256f94bdeea2e595560d
kent barnhurst <kentbarnhurst1@gmail.com>
2023-01-11 09:58:37 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnum = randomGenerator.Next(1, 100);
+
+        bool go = true;
+        while (go)
+        {
+            Console.Write("What is your Guess? ");
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+            if (guess > magicnum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicnum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                go = false;
+            }
+        }
     }
 }

1515c59dda07b08d11695917ceb08b7491094187
kent barnhurst <kentbarnhurst1@gmail.com>
2023-01-11 19:31:22 -0700

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        
+
+        bool go = true;
+        while (go)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+            else
+            {
+                go = false;
+            }
+        }
+        int sum = numbers.Sum();
+        int average = sum / numbers.Count;
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
     }
 }

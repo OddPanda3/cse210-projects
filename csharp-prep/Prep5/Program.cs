1a72044f1edf04af36e95636c1d76336fba4cbb4
kent barnhurst <kentbarnhurst1@gmail.com>
2023-01-11 19:52:54 -0700

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            number = SquareNumber(number);
+            Console.WriteLine($"{name}, the square of your number is {number}");
+
+        }
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        DisplayResult(name, num);
+
     }
 }
